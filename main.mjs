bd7fa290a8556b8dd0d6184c421da2aecda534c9
Victor Marin <marinuribe01@gmail.com>
2023-04-21 08:34:44 -0500

    starting

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import ReactDom from 'https://esm.sh/react-dom@18.2.0/client'
+
+const appDomElement = document.getElementById('app')
+
+const root = ReactDom.createRoot(appDomElement)
+root.render('Hola mijo')
+
